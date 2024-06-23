e20d7d1f0292403be634739211a92ef592865f9b
aditya6371 <adityadas9439@gmail.com>
2024-06-23 14:33:58 +0530

    Shared_pref Changes

--- lib/utils/shared_pref.dart
+++ lib/utils/shared_pref.dart
@@ -1,58 +1,57 @@
-import 'package:alldemo/Enum/enum.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPref {
   const SharedPref._();
 
   // Save Data
-  Future<bool> saveData(dynamic value, String key) async {
+  Future<bool> setValue(dynamic value, String key) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    switch (value.type) {
-      case DataType.boolType:
+    switch (value.runtimeType) {
+      case bool:
         return await prefs.setBool(key, value);
-      case DataType.doubleType:
+      case double:
         return await prefs.setDouble(key, value);
-      case DataType.intType:
+      case int:
         return await prefs.setInt(key, value);
-      case DataType.stringType:
+      case String:
         return await prefs.setString(key, value);
-      case DataType.listType:
+      case const (List<String>):
         return await prefs.setStringList(key, value);
       default:
-        throw ArgumentError("Invalid Error");
+        throw ArgumentError("Invalid data type");
     }
   }
 
   // Read Data
-  Future<bool> getUserBoolData(String prefkey) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getBool(prefkey) ?? false;
-  }
-  
-  Future<int> getUserIntData(String prefkey) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getInt(prefkey) ?? 0;
+  Future<bool> getBoolValue(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key) ?? false;
   }
 
-  Future<double> getUserDoubleData(String prefkey) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getDouble(prefkey) ?? 0.0;
+  Future<int> getIntValue(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(key) ?? 0;
   }
 
-  Future<String> getUserStringData(String prefkey) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getString(prefkey) ?? "";
+  Future<double> getDoubleValue(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getDouble(key) ?? 0.0;
   }
 
-  Future<List> getUserListData(String prefkey) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getStringList(prefkey) ?? [];
+  Future<String> getStringValue(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key) ?? "";
   }
 
-  //Delete Data
-  Future<void> removeUserData(String prefKey) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    await sharedPreferences.remove(prefKey);
+  Future<List<String>> getStringListValue(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(key) ?? [];
+  }
+
+  // Delete Data
+  Future<void> removeValue(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
   }
 }
